720ac4a8834f1451b69c78271ba0a16c72426800
scotth0828 <scotth0828@gmail.com>
2024-09-12 02:42:48 -0400

    small bug fix

--- DormBuddy/Controllers/HomeController.cs
+++ DormBuddy/Controllers/HomeController.cs
@@ -14,46 +14,41 @@ namespace DormBuddy.Controllers
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public HomeController(
-            ILogger<HomeController> logger,
-            UserManager<ApplicationUser> userManager,
-            SignInManager<ApplicationUser> signInManager)
-        {
-            _logger = logger;
-            _userManager = userManager;
-            _signInManager = signInManager;
-        }
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
 
-        public async Task<IActionResult> Index()
+    public IActionResult Index()
+    {
+        var username = HttpContext.Session.GetString("Username");
+        if (username != null)
         {
-            var user = await _userManager.GetUserAsync(User);
-            if (user != null)
-            {
-                return RedirectToAction("Dashboard", "Account");
-            }
-            return View();
+            return RedirectToAction("Dashboard", "Account");
         }
+        return View();
+    }
 
-        public async Task<IActionResult> HomeLogin()
+    public IActionResult HomeLogin()
+    {
+        var username = HttpContext.Session.GetString("Username");
+        if (username != null)
         {
-            var user = await _userManager.GetUserAsync(User);
-            if (user != null)
-            {
-                return RedirectToAction("Dashboard", "Account");
-            }
-            return View();
+            return RedirectToAction("Dashboard", "Account");
         }
+        return View();
+    }
 
         public IActionResult Privacy()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult ContactUs()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
