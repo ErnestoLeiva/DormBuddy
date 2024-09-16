109959b31029e5ff15702b5c6fda14a44e20840b
scotth0828 <scotth0828@gmail.com>
2024-09-16 19:27:45 -0400

    initial changes to account system

--- DormBuddy/Models/DBContext.cs
+++ DormBuddy/Models/DBContext.cs
@@ -4,6 +4,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DormBuddy.Models
 {
+
+    public class ApplicationUser : IdentityUser
+    {
+        public int Credits { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+
     public class ApplicationUser : IdentityUser
     {
         public int Credits { get; set; }
@@ -13,7 +21,7 @@ namespace DormBuddy.Models
 
     }
 
-    public class DBContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
+    public class DBContext : IdentityIdentityDbContext<ApplicationUser, IdentityRole, string><ApplicationUser, IdentityRole, string>
     {
         public DBContext(DbContextOptions<DBContext> options)
             : base(options)
@@ -96,5 +104,62 @@ namespace DormBuddy.Models
                 entity.Property(t => t.AssignedTo).HasMaxLength(160).IsRequired();
             });
         }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured) {
+                optionsBuilder.UseMySql("Server=shportfolio.net;Database=myportfolio_dormbuddy;User=myportfolio;Password=65eyqYcPHv;", 
+                    new MySqlServerVersion(new Version(8, 0, 2)));
+            }
+                
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Ensure the maximum length is set for all string properties
+            foreach (var entity in builder.Model.GetEntityTypes())
+            {
+                foreach (var property in entity.GetProperties())
+                {
+                    if (property.ClrType == typeof(string) && property.GetMaxLength() == null)
+                    {
+                        // Apply a default maximum length to avoid key length issues
+                        property.SetMaxLength(160);
+                    }
+                }
+            }
+
+            // Apply specific maximum length configurations for Identity tables
+            builder.Entity<ApplicationUser>(entity =>
+            {
+                entity.Property(m => m.Email).HasMaxLength(160);
+                entity.Property(m => m.NormalizedEmail).HasMaxLength(160);
+                entity.Property(m => m.UserName).HasMaxLength(160);
+                entity.Property(m => m.NormalizedUserName).HasMaxLength(160);
+                entity.Property(m => m.FirstName).HasMaxLength(160);
+                entity.Property(m => m.LastName).HasMaxLength(160);
+
+            });
+
+            builder.Entity<IdentityRole>(entity =>
+            {
+                entity.Property(m => m.Name).HasMaxLength(160);
+                entity.Property(m => m.NormalizedName).HasMaxLength(160);
+            });
+
+            // Specific configuration for IdentityUserLogin
+            builder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.Property(e => e.LoginProvider).HasMaxLength(160);
+                entity.Property(e => e.ProviderKey).HasMaxLength(160);
+                entity.Property(e => e.ProviderDisplayName).HasMaxLength(160);
+            });
+        }
+
+
     }
 }
