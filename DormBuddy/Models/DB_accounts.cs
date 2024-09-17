d953bed9a8808421ad73bbd5794d35edfe32f151
pamelamauna96 <96269164+pamelamauna96@users.noreply.github.com>
2024-09-16 20:31:01 -0400

    WIP - Admin Dashboard
    Dashboard for admins to provision other accounts

--- DormBuddy/Models/DB_accounts.cs
+++ DormBuddy/Models/DB_accounts.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DormBuddy.Models
 {
     public class DB_accounts
     {
-        public int Id { get; set; } // Primary key
-        public string AccountName { get; set; } = string.Empty;// Example property
-
+        [Key]
+        public int user_id { get; set; }
+        public required string email { get; set; }
+        public required string username { get; set; }
+        public required string password { get; set; }
+        public required string firstname { get; set; }
+        public required string lastname { get; set; }
+        public required int balance { get; set; }
+        public bool IsAdmin { get; set; }  // New property for admin check
     }
 }
