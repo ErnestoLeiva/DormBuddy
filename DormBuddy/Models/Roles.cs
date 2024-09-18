6dfa94f2ab0315c851fce4d6281a5ac07a7125ab
scotth0828 <scotth0828@gmail.com>
2024-09-18 19:16:02 -0400

    roles

---
+++ DormBuddy/Models/Roles.cs
@@ -0,0 +1,9 @@
+namespace DormBuddy.Models {
+
+    public static class Roles {
+        public const string Admin = "Admin";
+        public const string Moderator = "Moderator";
+        public const string User = "User";
+    }
+
+}
--- DormBuddy/Program.cs
+++ DormBuddy/Program.cs
@@ -8,11 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<DBContext>(options => 
-options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), 
-new MySqlServerVersion(new Version(8, 0, 2))));
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), 
+    new MySqlServerVersion(new Version(8, 0, 2))));
 
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<DBContext>().AddDefaultTokenProviders();
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<DBContext>()
+    .AddDefaultTokenProviders();
 
+#region PASSWORD REQ.
 builder.Services.Configure<IdentityOptions>(options =>
 {
     // Password settings
@@ -23,6 +26,7 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.Password.RequiredLength = 6;
     options.Password.RequiredUniqueChars = 1;
 });
+#endregion
 
 // Add session services
 builder.Services.AddSession(options =>
@@ -38,7 +42,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -56,4 +59,61 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=HomeLogin}/{id?}");
 
+await InitializeRolesAndAdminUser(app);
+
 app.Run();
+
+#region ROLES
+
+static async Task InitializeRolesAndAdminUser(WebApplication app)
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+        string[] roles = { Roles.Admin, Roles.Moderator, Roles.User };
+
+        foreach (var role in roles)
+        {
+            var roleExist = await roleManager.RoleExistsAsync(role);
+            if (!roleExist)
+            {
+                await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+
+        // Create a default admin account
+        var adminUser = new ApplicationUser
+        {
+            UserName = "admin",
+            Email = "admin@dormbuddy.com",
+            FirstName = "Admin",
+            LastName = "",
+            Credits = 0
+        };
+
+        string password = "Adminpass123!";
+
+        var user = await userManager.FindByEmailAsync(adminUser.Email);
+
+        if (user == null)
+        {
+            var createUserResult = await userManager.CreateAsync(adminUser, password);
+            if (createUserResult.Succeeded)
+            {
+                await userManager.AddToRoleAsync(adminUser, Roles.Admin);
+                Console.WriteLine("Admin account created successfully!");
+            }
+            else
+            {
+                Console.WriteLine("Error creating admin account!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Admin account already exists!");
+        }
+    }
+}
+#endregion
