3cfbcc6bb440aeebab158876f47a1d026fa304fa
scotth0828 <scotth0828@gmail.com>
2024-09-26 21:37:48 -0400

    small change to lockout timing

--- DormBuddy/Program.cs
+++ DormBuddy/Program.cs
@@ -23,8 +23,8 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
     options.SignIn.RequireConfirmedEmail = true;
 
     // lockout settings
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-    options.Lockout.MaxFailedAccessAttempts = 60;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(60);
+    options.Lockout.MaxFailedAccessAttempts = 5;
     options.Lockout.AllowedForNewUsers = true;
 })
     .AddEntityFrameworkStores<DBContext>()
