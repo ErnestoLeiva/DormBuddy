cf60f7a6c396e2628feb6b1e037cc3dd879c608b
Ernesto Leiva <eleiv009@fiu.edu>
2024-10-04 18:17:57 -0400

    Improved administration file structure and created new admin controller

--- DormBuddy/Controllers/AdministrationController.cs
+++ DormBuddy/Controllers/AdministrationController.cs
@@ -1,66 +1,16 @@
-using DormBuddy.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DormBuddy.Controllers
 {
     public class AdministrationController : Controller
     {
-        private readonly ILogger<AccountController> _logger;
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly IEmailSender _emailSender;
-
-        public AdministrationController(
-            ILogger<AccountController> logger,
-            UserManager<ApplicationUser> userManager,
-            IEmailSender emailSender)
-        {
-            _logger = logger;
-            _userManager = userManager;
-            _emailSender = emailSender;
-        }
-        
-        #region ADMIN/MODERATOR PANEL ACTIONS
-        
-        // GET: /Administration/AdminDashboard
+        // GET: /Administration/AdminPanel
         [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> AdminPanel()
-        {
-            if (User?.Identity?.IsAuthenticated == true)
-            {
-                var user = await _userManager.GetUserAsync(User);
-                if (user != null)
-                {
-                    ViewBag.Username = $"{user.FirstName} {user.LastName}";
-                }
-                return View("~/Views/Administration/AdminPanel.cshtml");
-            }
-            else
-            {
-                return RedirectToAction("AccessDenied");
-            }
-        }
+        public IActionResult AdminPanel() => View("~/Views/Administration/AdminPanel.cshtml");
 
-        // GET: /Administration/ModeratorDashboard
+        // GET: /Administration/ModeratorPanel
         [Authorize(Roles = "Admin,Moderator")]
-        public async Task<IActionResult> ModeratorPanel()
-        {
-            if (User?.Identity?.IsAuthenticated == true)
-            {
-                var user = await _userManager.GetUserAsync(User);
-                if (user != null)
-                {
-                    ViewBag.Username = $"{user.FirstName} {user.LastName}";
-                }
-                return View("~/Views/Administration/ModeratorPanel.cshtml");
-            }
-            else
-            {
-                return RedirectToAction("AccessDenied");
-            }
-        }
-        
-        #endregion
+        public IActionResult ModeratorPanel() => View("~/Views/Administration/ModeratorPanel.cshtml");
     }
 }
