2919795259d190a15f04440119ab38d907764d92
pamelamauna96 <96269164+pamelamauna96@users.noreply.github.com>
2024-10-21 22:24:05 -0400

    Grouping Model Created - handles fetching group data from the db

---
+++ DormBuddy/Models/GroupModel.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DormBuddy.Models
+{
+    public class GroupModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "GroupName is required.")]
+        public string GroupName { get; set; } = string.Empty;
+
+        public string CreatedBy { get; set; } = string.Empty; // UserId of the group creator
+
+        // Navigation property to hold the list of users in the group
+        public List<ApplicationUser> Users { get; set; } = new List<ApplicationUser>();
+    }
+}
