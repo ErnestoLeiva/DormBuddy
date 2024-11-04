020a064914d6a0ecdb98c8ef8d96d3ed9da4513a
pamelamauna96 <96269164+pamelamauna96@users.noreply.github.com>
2024-11-04 18:44:13 -0500

    Test: grouping functionality implemented

--- DormBuddy/Migrations/20241104015110_CreateUserGroupsTable.Designer.cs
+++ DormBuddy/Migrations/20241104015110_CreateUserGroupsTable.Designer.cs
@@ -4,6 +4,7 @@ using DormBuddy.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DormBuddy.Migrations
 {
     [DbContext(typeof(DBContext))]
-    partial class DBContextModelSnapshot : ModelSnapshot
+    [Migration("20241104015110_CreateUserGroupsTable")]
+    partial class CreateUserGroupsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -99,6 +102,10 @@ namespace DormBuddy.Migrations
                         .HasColumnType("tinyint(1)");
 
                     b.Property<string>("SecurityStamp")
+                        .HasMaxLength(160)
+                        .HasColumnType("varchar(160)");
+
+                    b.Property<string>("TimeZone")
                         .HasMaxLength(160)
                         .HasColumnType("varchar(160)");
 
