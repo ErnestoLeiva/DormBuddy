2f704f94772c38e0db661eea9d6ce9e0fbb9c8cf
pamelamauna96 <96269164+pamelamauna96@users.noreply.github.com>
2024-11-25 21:13:38 -0500

    Fix null reference error for ViewBag.Roles in Dashboard view

---
+++ DormBuddy/Migrations/20241126003710_AddLastLoginDate.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DormBuddy.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLastLoginDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserProfiles_AspNetUsers_UserId",
+                table: "UserProfiles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "UserProfiles",
+                type: "varchar(160)",
+                maxLength: 160,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(160)",
+                oldMaxLength: 160)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "UserProfiles",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLoginDate",
+                table: "AspNetUsers",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TemporaryProperty",
+                table: "AspNetUsers",
+                type: "varchar(160)",
+                maxLength: 160,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TimeZone",
+                table: "AspNetUsers",
+                type: "varchar(160)",
+                maxLength: 160,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalLogins",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    GroupName = table.Column<string>(type: "varchar(160)", maxLength: 160, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CreatedBy = table.Column<string>(type: "varchar(160)", maxLength: 160, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "UserGroups",
+                columns: table => new
+                {
+                    GroupsId = table.Column<int>(type: "int", nullable: false),
+                    MembersId = table.Column<string>(type: "varchar(160)", maxLength: 160, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserGroups", x => new { x.GroupsId, x.MembersId });
+                    table.ForeignKey(
+                        name: "FK_UserGroups_AspNetUsers_MembersId",
+                        column: x => x.MembersId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserGroups_Groups_GroupsId",
+                        column: x => x.GroupsId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserGroups_MembersId",
+                table: "UserGroups",
+                column: "MembersId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserProfiles_AspNetUsers_UserId",
+                table: "UserProfiles",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserProfiles_AspNetUsers_UserId",
+                table: "UserProfiles");
+
+            migrationBuilder.DropTable(
+                name: "UserGroups");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropColumn(
+                name: "LastLoginDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "TemporaryProperty",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "TimeZone",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "TotalLogins",
+                table: "AspNetUsers");
+
+            migrationBuilder.UpdateData(
+                table: "UserProfiles",
+                keyColumn: "UserId",
+                keyValue: null,
+                column: "UserId",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "UserProfiles",
+                type: "varchar(160)",
+                maxLength: 160,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(160)",
+                oldMaxLength: 160,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "UserProfiles",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserProfiles_AspNetUsers_UserId",
+                table: "UserProfiles",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
