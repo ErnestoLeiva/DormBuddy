e42079823a88c77332d4d3b3982877dad818936d
pamelamauna96 <96269164+pamelamauna96@users.noreply.github.com>
2024-11-27 19:50:01 -0500

    Added migrations_backup directory

---
+++ migrations_backup/20241123022311_AdditionToDashChat.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DormBuddy.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdditionToDashChat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "type",
+                table: "DashboardChatModel",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DashboardChatModel_UserId",
+                table: "DashboardChatModel",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DashboardChatModel_AspNetUsers_UserId",
+                table: "DashboardChatModel",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DashboardChatModel_AspNetUsers_UserId",
+                table: "DashboardChatModel");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DashboardChatModel_UserId",
+                table: "DashboardChatModel");
+
+            migrationBuilder.DropColumn(
+                name: "type",
+                table: "DashboardChatModel");
+        }
+    }
+}
