3e3b5bd0bbdfa56f84575cbf58d5b7a166254473
scotth0828 <scotth0828@gmail.com>
2024-11-28 03:16:44 -0500

    small fix to database schema

---
+++ DormBuddy/Migrations/20241128081605_Profile_Posts.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DormBuddy.Migrations
+{
+    /// <inheritdoc />
+    public partial class Profile_Posts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Profile_Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<string>(type: "varchar(160)", maxLength: 160, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Message = table.Column<string>(type: "varchar(160)", maxLength: 160, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Reply_Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profile_Posts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Profile_Posts_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profile_Posts_UserId",
+                table: "Profile_Posts",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Profile_Posts");
+        }
+    }
+}
