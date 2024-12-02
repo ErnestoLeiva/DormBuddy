9ff2857ae454bfa291a22d88a888f62517afcf1f
pamelamauna96 <96269164+pamelamauna96@users.noreply.github.com>
2024-12-01 23:16:36 -0500

    Updated migrations folder from main

---
+++ DormBuddy/Migrations/20241122040427_Changes.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DormBuddy.Migrations
+{
+    /// <inheritdoc />
+    public partial class Changes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AddColumn<string>(
+                name: "BannerImageUrl",
+                table: "UserProfiles",
+                type: "VARCHAR(160)",
+                nullable: true);
+
+            migrationBuilder.DropColumn(
+                name: "AccountStatus",
+                table: "UserProfiles"
+            );
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "UserProfiles",
+                type: "TEXT",
+                maxLength: 160,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(160)",
+                oldMaxLength: 160,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Verified",
+                table: "UserProfiles",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "DashboardChatModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    sent_at = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    message = table.Column<string>(type: "TEXT", maxLength: 160, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DashboardChatModel", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "FriendsModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    FriendId = table.Column<int>(type: "int", nullable: false),
+                    blocked = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FriendsModel", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.DropTable(
+                name: "DashboardChatModel");
+
+            migrationBuilder.DropTable(
+                name: "FriendsModel");
+
+            migrationBuilder.DropColumn(
+                name: "Verified",
+                table: "UserProfiles");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AccountStatus",
+                table: "UserProfiles",
+                type: "varchar(160)",
+                nullable: true
+            );
+
+            migrationBuilder.DropColumn(
+                name: "BannerImageUrl",
+                table: "UserProfiles"
+            );
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "UserProfiles",
+                type: "varchar(160)",
+                maxLength: 160,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 160,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
